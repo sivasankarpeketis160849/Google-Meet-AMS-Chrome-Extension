26f24134330a9fd60839595bb2637515fe1e321b
CaptainBATMAN <myselfgeek01@gmail.com>
2021-07-16 18:04:59 +0530

    fixed error and success messages in popup..

--- Popup/popup.js
+++ Popup/popup.js
@@ -1,10 +1,22 @@
 document.addEventListener('DOMContentLoaded', (event) => {
+    
+    // if user clicked on "Start Monitoring"
     startMonitoring.addEventListener('click', () => {
+
+        if(!($("#refreshError").hasClass('d-none'))){
+            $("#refreshError").addClass('d-none');
+        }
+        if(!($("#monitoringSuccess").hasClass('d-none'))){
+            $("#monitoringSuccess").addClass('d-none');
+            
+        }
+
         if(!($('#refresh').val()<=60 && $('#refresh').val()>=5)){
             $("#refreshError").removeClass('d-none');
             return;
         }
 
+        $('#monitoringSuccess').removeClass('d-none');
         var date = new Date();
         var startTimeMillis = date.getTime();
         var startTime = moment(startTimeMillis,"x").format("DD-MM-YY HH:MM A");
